08733566216cbbdc98f0cfbff06281f7064fd405
spottyunderdog <spottyunderdog18@gmail.com>
2026-08-17 22:45:43 -0400

    testing homemanager file again

--- modules/features/homemanager/homemanager.nix
+++ modules/features/homemanager/homemanager.nix
@@ -1,9 +1,8 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, home-manager,... }: {
     imports = [
       #app config files
     ];
-
     home.username = "spotty";
+    home.stateVersion="26.05";
     home.directory = "/home/spotty";
-    home.stateVersion = "26.05";
 }
