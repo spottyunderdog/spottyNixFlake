1851d5b9a13a9def01d67fa0155590097a81d097
spottyunderdog <spottyunderdog18@gmail.com>
2026-08-18 16:11:09 -0400

    Fixed various config errors

--- modules/features/homemanager/configs.nix
+++ modules/features/homemanager/configs.nix
@@ -1,9 +1,9 @@
 { self, inputs, ... }: {
   flake.homeModules.appConfigs = { pkgs, lib, config, ... }: {
-    imports = {
+    imports = [
       # Home Manager app Config files
       self.homeModules.zed
-    }
+    ];
     zedConfig.enable = lib.mkDefault false;
   };
 }
--- modules/hosts/schoolLaptop/home.nix
+++ modules/hosts/schoolLaptop/home.nix
@@ -16,9 +16,9 @@
   # This is your home.nix, your module where you configure home-manager
   # It's imported both in standalone configuration above, and in your nixos configuration
   flake.homeModules.spottyModule = { pkgs, ... }: {
-    imports = {
+    imports = [
       self.homeModules.appConfigs
-    };
+    ];
     zedConfig.enable = true;
     programs.bash.enable = true;
     programs.bash.shellAliases.ll = "ls -l";
