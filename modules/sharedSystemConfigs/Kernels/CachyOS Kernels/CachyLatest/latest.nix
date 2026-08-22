b9296a7ef0613bbe404c62e2bd9beb7cec28e096
spottyunderdog <spottyunderdog17@gmail.com>
2026-08-21 20:12:19 -0400

    Created a module for the x86-v3 version of the latest cachy kernel

--- modules/sharedSystemConfigs/Kernels/CachyOS Kernels/CachyLatest/latest.nix
+++ modules/sharedSystemConfigs/Kernels/CachyOS Kernels/CachyLatest/latest.nix
@@ -6,5 +6,10 @@
     config = lib.mkIf config.cachyLatestKernel.enable {
       boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
     };
+    imports = [
+      nixosModules.cachyLatest-x86v3
+    ];
+    # Architechture Versions, Only enable if your know what your doing
+    cachyLatest-x86v3Kernel.enable = lib.mkDefault false;
   };
 }
